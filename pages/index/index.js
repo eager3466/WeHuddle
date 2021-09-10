dda2352edaf65ea64ef06c5eab7e9ca296bf051b
yuhuaqing <yuhuaqing@xiaomi.com>
2021-09-10 10:32:38 +0800

    [Development] Init commit by hanling.
    Signed-off-by: yuhuaqing <yuhuaqing@xiaomi.com>

---
+++ pages/index/index.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }],
+
+  },
+  // 点击抱团
+  team:function(){
+    wx.navigateTo({
+      url: '/pages/team/team',
+    })
+  }
+})
