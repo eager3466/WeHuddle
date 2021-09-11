c5b87cde0fbc47357be5df1f3db800d2c06aa192
lanshilei <531589402@qq.com>
2021-09-11 10:07:39 +0800

    活动详情

---
+++ pages/detail/detail.js
@@ -0,0 +1,18 @@
+const app = getApp();
+Page({
+  data: {
+    item: {
+      imageUrl:'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg',
+      description: "折磨生出苦难，苦难又会加剧折磨，凡间这无穷的循环，将有我来终结！真正的恩典因不完整而美丽，因情感而真诚，因脆弱而自由！",
+      sponsor: "小米技术委员会",
+      startDate: "2021-09-10",
+      endDate: "2021-09-12",
+      requirement: "五人成团",
+      website: "i.mi.com"
+    },
+  },
+  signup: function() {
+  },
+  group: function() {
+  }
+})
